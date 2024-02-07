7f9a6439e24cf1e79b82aab0207529b52dd048c9
Josh Mendoza <jacinto.josh.mendoza@gmail.com>
2024-02-06 20:09:28 -0600

    Added bulma and deleted files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css';
+import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png'
+import CortanaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Personal Digital Assistants</div>
+
+
+      {/* <img src={AlexaImage} />
+      <img src={SiriImage} /> */}
+
+      {/* <img src='https://thispersondoesnotexist.com' /> */}
+
+      {/* Props to move from Parent to Child
+      App.js -> ProfileCard.js */}
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} useless="child doesn't use this"/>
+      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage}/>
+      <ProfileCard title="Siri" handle="@AppleSiri" image={SiriImage}/>
     </div>
   );
 }
---
+++ src/ProfileCard.js
@@ -0,0 +1,16 @@
+function ProfileCard({title, handle, image}) {
+
+    // const title = props.title;
+    // const handle = props.handle;
+    // const {title, handle} = props;
+
+    return (
+    <div>
+        <img src={image} alt="pda logo"/>
+        <div>Title is {title}</div>
+        <div>Handle is {handle}</div>
+    </div>
+    );
+}
+
+export default ProfileCard;
